4c40b2173a11453f0d5b6c7ccdd2b3706a773e92
Maciej Kortas <mackor002@utp.edu.pl>
2019-11-17 14:55:23 +0100

    dalsza czesc podstaw gier

--- FlappyBird/Assets/Scripts/Level.cs
+++ FlappyBird/Assets/Scripts/Level.cs
@@ -2,52 +2,152 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class Level : MonoBehaviour
 {
     private const float CAMERA_ORTHO_SIZE =50f;
     private const float PIPE_WIDTH = 7.8f;
     private const float PIPE_HEAD_HEIGHT = 4f;
-    private const float PIPE_MOVE_SPEED = 20f;
+    private const float PIPE_MOVE_SPEED = 30f;
+    private const float PIPE_DESTROY_X_POSITION = -115f;
+    private const float PIPE_SPAWN_X_POSITION = +115f;
+    private const float BIRD_X_POSITION = 0f;
+
+    private static Level instance;
+
+    public static Level GetInstance()
+    {
+        return instance;
+    }
 
     private List<Pipe> pipeList;
+    private int pipesPassedCount;
+    private int pipesSpawned;
+    private float pipeSpawnTimer;
+    private float pipeSpawnTimerMax;
+    private float gapSize;
+    private State state;
+
+    public enum Difficulty
+    {
+        Easy,
+        Medium,
+        Hard,
+        Impossible,
+    }
+
+    private enum State {
+    Playing,
+    BirdDead,  
+    }
 
     private void Awake()
     {
+        instance = this;
         pipeList = new List<Pipe>();
-
-        
+        pipeSpawnTimerMax = 1f;
+        SetDifficulty(Difficulty.Easy);
+        state = State.Playing;
     }
     private void Start()
 
     {
-        CreateGapPipes(50f, 20f, 20f);
-       
-        
+        // CreateGapPipes(50f, 20f, 20f);      
+        Uni.GetInstance().OnDied += Uni_OnDied;
+    }
+
+    private void Uni_OnDied(object sender, System.EventArgs e)
+    {
+        //  CMDebug.TextPopupMouse("dead");
+        state = State.BirdDead;
     }
 
     private void Update()
     {
+        if(state == State.Playing) { 
         HandlePipeMovement();
+        HandlePipeSpawning();
+        }
+    }
+    private void HandlePipeSpawning() {
+        pipeSpawnTimer -= Time.deltaTime;
+        if(pipeSpawnTimer < 0)
+        {
+            //Time to spawn another pipe
+            pipeSpawnTimer += pipeSpawnTimerMax;
+
+            float heightEdgeLimit = 10f;
+            float minHeight = gapSize * .5f + heightEdgeLimit;
+            float totalHeight = CAMERA_ORTHO_SIZE * 2f;
+            float maxHeight = totalHeight - gapSize * .5f - heightEdgeLimit;
+
+            float height = Random.Range(minHeight, maxHeight);
+            CreateGapPipes(height, gapSize, PIPE_SPAWN_X_POSITION);
+        }
     }
 
-    private  void HandlePipeMovement()
-    {
-        foreach (Pipe pipe in pipeList)
-        {
+    private  void HandlePipeMovement() {
+
+        for(int i=0;i<pipeList.Count;i++){
+            Pipe pipe = pipeList[i];
+            bool isToTheRightOfBird = pipe.GetXPosition() > BIRD_X_POSITION;
             pipe.Move();
+            if(isToTheRightOfBird && pipe.GetXPosition() <= BIRD_X_POSITION && pipe.IsBottom())
+            {
+                //Pipe passed Bird
+                pipesPassedCount++;
+            }
+            if(pipe.GetXPosition() < PIPE_DESTROY_X_POSITION)           {
+                // Destroy pipe
+                pipe.DestroySelf();
+                pipeList.Remove(pipe);
+                i--;
+            }
         }
+    }
+
+    private void SetDifficulty(Difficulty difficulty)
+    {
+        switch (difficulty)        {
+            case Difficulty.Easy:
+                gapSize = 50f;
+                pipeSpawnTimerMax = 1.2f;
+                break;
+            case Difficulty.Medium:
+                gapSize = 40f;
+                pipeSpawnTimerMax = 1.1f;
+                break;
+            case Difficulty.Hard:
+                gapSize = 33f;
+                pipeSpawnTimerMax = 1.0f;
+                break;
+            case Difficulty.Impossible:
+                gapSize = 24f;
+                pipeSpawnTimerMax = .8f;
+                break;                 
+        }
+    }
+    private Difficulty GetDifficulty()
+    {
+        if (pipesSpawned >= 30) return Difficulty.Impossible;
+        if (pipesSpawned >= 20) return Difficulty.Hard;
+        if (pipesSpawned >= 10) return Difficulty.Medium;
+        return Difficulty.Easy;
     }
     private void CreateGapPipes(float gapY, float gapSize, float xPosition)
     {
         CreatePipe(gapY - gapSize * .5f,xPosition,true);
         CreatePipe(CAMERA_ORTHO_SIZE * 2f - gapY - gapSize * .5f, xPosition, false);
+        pipesSpawned++;
+        SetDifficulty(GetDifficulty());
     }
-    private void CreatePipe(float height, float xPosition, bool createBottom)
+    private void CreatePipe(float height, float xPosition, bool isBottom)
     {
         //Set up Pipe  Head
         Transform pipeHead = Instantiate(GameAssets.GetInstance().pfPipeHead);
         float pipeHeadYPosition;
-        if (createBottom) {
+        if (isBottom) {
             pipeHeadYPosition = -CAMERA_ORTHO_SIZE + height - PIPE_HEAD_HEIGHT * .5f;
         }
         else
@@ -59,7 +159,7 @@ public class Level : MonoBehaviour
         //Set up Pipe  Body
         Transform pipeBody = Instantiate(GameAssets.GetInstance().pfPipeBody);
         float pipeBodyYPosition;
-        if (createBottom)
+        if (isBottom)
         {
             pipeBodyYPosition = -CAMERA_ORTHO_SIZE;
 
@@ -78,26 +178,52 @@ public class Level : MonoBehaviour
         pipeBodyBoxColider.size = new Vector2(PIPE_WIDTH, height);
         pipeBodyBoxColider.offset = new Vector2(0f, height * .5f);
 
-        Pipe pipe = new Pipe(pipeHead, pipeBody);
+        Pipe pipe = new Pipe(pipeHead, pipeBody, isBottom);
         pipeList.Add(pipe);
             }
+    public int GetPipesSpawned()
+    {
+        return pipesSpawned;
+    }
 
+    public int GetPipesPassedCount()
+    {
+        return pipesPassedCount;
+    }
 
     //Represents a single Pipe
     private class Pipe
     {
         private Transform pipeHeadTransform;
         private Transform pipeBodyTransform;
+        private bool isBottom;
 
-        public Pipe(Transform pipeHeadTransform, Transform pipeBodyTransform)
+        public Pipe(Transform pipeHeadTransform, Transform pipeBodyTransform, bool isBottom)
         {
             this.pipeHeadTransform = pipeHeadTransform;
             this.pipeBodyTransform = pipeBodyTransform;
+            this.isBottom = isBottom;
         }
         public void Move()
         {
             pipeHeadTransform.position += new Vector3(-1, 0, 0) * PIPE_MOVE_SPEED * Time.deltaTime;
-            pipeBodyTransform.position += new Vector3(-1, 0, 0) * PIPE_MOVE_SPEED * Time.deltaTime;       
+            pipeBodyTransform.position += new Vector3(-1, 0, 0) * PIPE_MOVE_SPEED * Time.deltaTime;
+        }
+        public float GetXPosition()
+        {
+            return pipeHeadTransform.position.x;
+        }
+
+        public bool IsBottom()
+        {
+            return isBottom;
+        }
+
+
+        public void DestroySelf()
+        {
+            Destroy(pipeHeadTransform.gameObject);
+            Destroy(pipeBodyTransform.gameObject);
         }
     }
    
--- FlappyBird/Assets/Scripts/Uni.cs
+++ FlappyBird/Assets/Scripts/Uni.cs
@@ -1,14 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+
 
 public class Uni : MonoBehaviour
 {
     private const float JUMP_AMOUNT = 80f;
 
+    
+    private static Uni instance;
+    public static Uni GetInstance()
+    {
+        return instance;
+
+    }
+
+    public event EventHandler OnDied;
+
     private Rigidbody2D Unirigidbody2D;
+
     private void Awake()
     {
+        instance = this;
         Unirigidbody2D = GetComponent<Rigidbody2D>();
     }
   
@@ -26,6 +40,7 @@ public class Uni : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collider)
     {
-
+        Unirigidbody2D.bodyType = RigidbodyType2D.Static;
+        if (OnDied != null) OnDied(this, EventArgs.Empty);
     }
 }
