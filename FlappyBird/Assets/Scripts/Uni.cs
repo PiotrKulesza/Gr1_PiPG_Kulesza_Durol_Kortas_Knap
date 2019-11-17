74dccf72aee879141defdb1f60c867fa8547dec6
MarcinKna12 <marcin_knap@onet.pl>
2019-11-17 09:41:36 +0100

    Podstawy gry

---
+++ FlappyBird/Assets/Scripts/Uni.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Uni : MonoBehaviour
+{
+    private const float JUMP_AMOUNT = 80f;
+
+    private Rigidbody2D Unirigidbody2D;
+    private void Awake()
+    {
+        Unirigidbody2D = GetComponent<Rigidbody2D>();
+    }
+  
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+    private void Jump()
+    {
+        Unirigidbody2D.velocity = Vector2.up * JUMP_AMOUNT;
+
+    }
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+
+    }
+}
