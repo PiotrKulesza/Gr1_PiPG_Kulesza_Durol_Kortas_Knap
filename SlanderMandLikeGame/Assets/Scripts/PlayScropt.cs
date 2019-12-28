17f1f4f1634f51f1fe5635f422e2e1069b1c5f60
PiotrKulesza <piotrkulesza16@onet.pl>
2019-12-28 16:17:15 +0100

    Dokończenie terenuy oraz dodanie menu

---
+++ SlanderMandLikeGame/Assets/Scripts/PlayScropt.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayScropt : MonoBehaviour
+{
+
+    public Color OnMouseOverColor = Color.red;
+    public Color OnMouseExitColor = Color.white;
+
+   
+
+
+    void OnMouseOver()
+    {
+        GetComponent<TextMesh>().color = OnMouseOverColor;
+    }
+    void OnMouseExit()
+    {
+        GetComponent<TextMesh>().color = OnMouseExitColor;
+    }
+
+    void OnMouseUp()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+      
+        
+    }
+}
